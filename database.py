ea91dfe7393a7d4825a5d88bef5136736c2fcfae
CodeSandbox <support@codesandbox.io>
2023-12-09 08:32:10 +0000

    Sandbox snapshot commit

---
+++ database.py
@@ -0,0 +1,129 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import inspect
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///POS.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+class Product(db.Model):
+    try:
+        product_id = db.Column(db.Integer, primary_key=True)
+        product_name = db.Column(db.Text, nullable=False)
+        price = db.Column(db.Float, nullable=False)
+        stock_quantity = db.Column(db.Integer, nullable=False)
+        barcode = db.Column(db.Text, unique=True)
+        category = db.Column(db.Text)
+    except Exception as e:
+        print(f"Error in Product model: {e}")
+
+
+class Customer(db.Model):
+    try:
+        customer_id = db.Column(db.Integer, primary_key=True)
+        customer_name = db.Column(db.Text, nullable=False)
+        email = db.Column(db.Text)
+        phone_number = db.Column(db.Text)
+        address = db.Column(db.Text)
+    except Exception as e:
+        print(f"Error in Customer model: {e}")
+
+
+class Sale(db.Model):
+    try:
+        sale_id = db.Column(db.Integer, primary_key=True)
+        customer_id = db.Column(
+            db.Integer, db.ForeignKey('customer.customer_id'))
+        sale_date = db.Column(
+            db.TIMESTAMP, server_default=db.func.current_timestamp())
+        total_amount = db.Column(db.Float, nullable=False)
+        payment_method = db.Column(db.Text)
+        notes = db.Column(db.Text)
+        user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+        customer = db.relationship('Customer', backref='sales')
+        user = db.relationship('User', backref='sales')
+    except Exception as e:
+        print(f"Error in Sale class: {e}")
+
+
+class SaleItem(db.Model):
+    try:
+        sale_item_id = db.Column(db.Integer, primary_key=True)
+        sale_id = db.Column(db.Integer, db.ForeignKey('sale.sale_id'))
+        product_id = db.Column(db.Integer, db.ForeignKey('product.product_id'))
+        quantity = db.Column(db.Integer, nullable=False)
+        unit_price = db.Column(db.Float, nullable=False)
+        item_amount = db.Column(db.Float, nullable=False)
+        sale = db.relationship('Sale', backref='items')
+        product = db.relationship('Product', backref='sales')
+    except Exception as e:
+        print(f"Error in SaleItem class: {e}")
+
+
+class User(db.Model):
+    try:
+        user_id = db.Column(db.Integer, primary_key=True)
+        username = db.Column(db.Text, nullable=False, unique=True)
+        password_hash = db.Column(db.Text, nullable=False)
+        role = db.Column(db.Text, default='user')
+    except Exception as e:
+        print(f"Error in User class: {e}")
+
+
+class Order(db.Model):
+    try:
+        order_id = db.Column(db.Integer, primary_key=True)
+        customer_id = db.Column(
+            db.Integer, db.ForeignKey('customer.customer_id'))
+        order_date = db.Column(
+            db.TIMESTAMP, server_default=db.func.current_timestamp())
+        total_amount = db.Column(db.Float, nullable=False)
+        payment_method = db.Column(db.Text)
+        notes = db.Column(db.Text)
+        user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'))
+        customer = db.relationship('Customer', backref='orders')
+        user = db.relationship('User', backref='orders')
+    except Exception as e:
+        print(f"Error in Order class: {e}")
+
+
+class OrderItem(db.Model):
+    try:
+        order_item_id = db.Column(db.Integer, primary_key=True)
+        order_id = db.Column(db.Integer, db.ForeignKey('order.order_id'))
+        product_id = db.Column(db.Integer, db.ForeignKey('product.product_id'))
+        quantity = db.Column(db.Integer, nullable=False)
+        unit_price = db.Column(db.Float, nullable=False)
+        item_amount = db.Column(db.Float, nullable=False)
+        order = db.relationship('Order', backref='items')
+        product = db.relationship('Product', backref='orders')
+    except Exception as e:
+        print(f"Error in OrderItem class: {e}")
+
+
+if __name__ == '__main__':
+    db_file_path = 'POS.sqlite'
+
+    try:
+        with app.app_context():
+            inspector = inspect(db.engine)
+
+            if not os.path.exists(db_file_path):
+                # Create the tables
+                db.create_all()
+                print("Database file created, and tables created successfully.")
+            else:
+                # Check if each table exists, and create it if not
+                for table_name in db.metadata.tables.keys():
+                    if not inspector.get_table_names().__contains__(table_name):
+                        db.create_all()
+                        print(f"Table '{table_name}' created successfully.")
+                    else:
+                        print(
+                            f"Table '{table_name}' already exists. Skipping table creation.")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
--- main.py
+++ main.py
@@ -1,12 +1,61 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template, url_for
+from flask_restful import Api, Resource
+from flask_sqlalchemy import SQLAlchemy
+from database import db, Product  # Import Product from the database module
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///POS.sqlite"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+api = Api(app)
+db.init_app(app)
 
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+class ProductResource(Resource):
+    def get(self, product_id=None):
+        if product_id:
+            product = Product.query.get(product_id)
+            if product:
+                return jsonify({
+                    'product_id': product.product_id,
+                    'product_name': product.product_name,
+                    'price': product.price,
+                    'stock_quantity': product.stock_quantity,
+                    'barcode': product.barcode,
+                    'category': product.category
+                })
+            else:
+                return jsonify({'message': 'Product not found'}), 404
+        else:
+
+            products = Product.query.all()
+            product_list = [{
+                'product_id': p.product_id,
+                'product_name': p.product_name,
+                'price': p.price,
+                'stock_quantity': p.stock_quantity,
+                'barcode': p.barcode,
+                'category': p.category
+            } for p in products]
+            return jsonify(product_list)
 
 
-if __name__ == "__main__":
-    app.run()
+api.add_resource(ProductResource, '/product', '/product/<int:product_id>')
+
+
+@app.route('/api/data')
+def get_data():
+    data = {'status': 'success', 'message': 'Data fetched successfully'}
+    return jsonify(data)
+
+
+@app.route('/')
+def render_frontend():
+    api_response = {'status': 'success', 'message': 'API data endpoint'}
+    return render_template('index.html', api_data=api_response, css_url=url_for('static', filename='/style.css'))
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+
+    app.run(host='0.0.0.0', port=5000, debug=True)
