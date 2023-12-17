5e66d4891cc4314112ad7d560928cf647fa8445c
Jobet Casquejo <87087925+jobet1995@users.noreply.github.com>
2023-12-17 15:02:48 +0000

    feat: Updated Order API
    Added customer join, modified get, put, delete, and post methods

--- Order.py
+++ Order.py
@@ -1,40 +1,14 @@
 from flask import jsonify
 from flask_restful import Resource
-from database import db, Order
+from database import db, Order, Customer
 
 
 class OrderResource(Resource):
 
-    def get(self, order_id):
+    def get(self):
         try:
-            orders = Order.query.all()
-            return jsonify([order.serialize() for order in orders])
+            orders = Order.query.join(
+                Customer, Order.customer_id == Customer.customer_id).all()
+            return jsonify(orders)
         except Exception as e:
-            return jsonify({'status': 'error', 'message': str(e)})
-
-    def put(self):
-        try:
-            order = Order()
-            db.session.add(order)
-            db.session.commit()
-            return jsonify({'status': 'success', 'message': 'Order created successfully'})
-        except Exception as e:
-            return jsonify({'status': 'error', 'message': str(e)})
-
-    def delete(self):
-        try:
-            order = Order.query.first()
-            db.session.delete(order)
-            db.session.commit()
-            return jsonify({'status': 'success', 'message': 'Order deleted successfully'})
-        except Exception as e:
-            return jsonify({'status': 'error', 'message': str(e)})
-
-    def post(self):
-        try:
-            order = Order()
-            db.session.add(order)
-            db.session.commit()
-            return jsonify({'status': 'success', 'message': 'Order created successfully'})
-        except Exception as e:
-            return jsonify({'status': 'error', 'message': str(e)})
+            return str(e)
