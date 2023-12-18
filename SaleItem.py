93ff0ee899b98b0f236afe3d082b06e7284ddf96
Jobet Casquejo <87087925+jobet1995@users.noreply.github.com>
2023-12-18 05:59:37 +0000

    feat: Add method to add new products for sale
    Adds the ability to add new products for sale and returns the updated list of sale items

--- SaleItem.py
+++ SaleItem.py
@@ -49,3 +49,30 @@ class SaleItemResource(Resource):
             return jsonify(sale_items)
         except Exception as e:
             return str(e)
+
+    def put(self):
+        try:
+            sale_items = SaleItem.query(SaleItem, Product).join(
+                Product, SaleItem.product_id == Product.product_id).all()
+            sale_items = [{
+                'sale_item_id': sale_item[0].sale_item_id,
+                'sale_id': sale_item[0].sale_id,
+                'product_id': sale_item[0].product_id,
+                'product_name': sale_item[1].product_name,
+                'quantity': sale_item[0].quantity,
+                'price': sale_item[0].price
+            }for sale_item in sale_items]
+
+            product = Product(
+                product_name=request.json['product_name'],
+                price=request.json['price'],
+                stock_quantity=request.json['stock_quantity'],
+                barcode=request.json['barcode'],
+                category=request.json['category']
+            )
+
+            db.session.add(product)
+            db.session.commit()
+            return jsonify(sale_items)
+        except Exception as e:
+            return str(e)
