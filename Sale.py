f881ad8b7810f68ab8f49150291fbe1ca7b8fb43
Jobet Casquejo <87087925+jobet1995@users.noreply.github.com>
2023-12-18 01:24:39 +0000

    feat: Add sale retrieval and product creation logic
    Added code for retrieving sales with customer and product information and creating a new product.

--- Sale.py
+++ Sale.py
@@ -50,3 +50,30 @@ class SaleResource(Resource):
             db.session.commit()
         except Exception as e:
             return str(e)
+
+    def put(self):
+        try:
+            sales = Sale.query(Sale, Customer, Product).join(
+                Customer, Sale.customer_id == Customer.customer_id).join(
+                Product, Sale.product_id == Product.product_id).all()
+            sales = [{
+                'sale_id': sale[0].sale_id,
+                'customer_id': sale[0].customer_id,
+                'customer_name': sale[1].customer_name,
+                'product_id': sale[0].product_id,
+                'product_name': sale[2].product_name,
+                'quantity': sale[0].quantity,
+                'price': sale[0].price
+            }for sale in sales]
+
+            product = Product(
+                product_name=request.json['product_name'],
+                price=request.json['price'],
+                stock_quantity=request.json['stock_quantity'],
+                barcode=request.json['barcode'],
+                category=request.json['category']
+            )
+            db.session.add(product)
+            db.session.commit()
+        except Exception as e:
+            return str(e)
