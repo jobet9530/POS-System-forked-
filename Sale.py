f9990a50c019aa496e93751fdfdf9a5d341b166c
Jobet Casquejo <87087925+jobet1995@users.noreply.github.com>
2023-12-18 00:50:58 +0000

    refactor: Update Sale.py
    Updated the database query and data structure in Sale.py

--- Sale.py
+++ Sale.py
@@ -1,106 +1,52 @@
 from flask import jsonify, request
 from flask_restful import Resource
-from database import db, Sale, Product
+from database import db, Sale, Customer, Product
 
 
 class SaleResource(Resource):
 
     def get(self):
         try:
-            sales = db.session.query(Sale, Product).join(
+            sales = Sale.query(Sale, Customer, Product).join(
+                Customer, Sale.customer_id == Customer.customer_id).join(
                 Product, Sale.product_id == Product.product_id).all()
-            sales_list = [{
+            sales = [{
                 'sale_id': sale[0].sale_id,
                 'customer_id': sale[0].customer_id,
+                'customer_name': sale[1].customer_name,
                 'product_id': sale[0].product_id,
-                'sales_date': sale[0].sales_date,
-                'total_amount': sale[0].total_amount,
-                'payment_method': sale[0].payment_method,
-                'notes': sale[0].notes,
-                'product': {
-                    'product_id': sale[1].product_id,
-                    'product_name': sale[1].product_name,
-                    'price': sale[1].price,
-                    'quantity': sale[1].quantity
-                }
+                'product_name': sale[2].product_name,
+                'quantity': sale[0].quantity,
+                'price': sale[0].price
             }for sale in sales]
-            return jsonify(sales_list)
+
+            return jsonify(sales)
         except Exception as e:
-            return {'error': str(e)}
+            return str(e)
 
     def post(self):
         try:
-            sales = []
-            customer_id = request.json['customer_id']
-            product_id = request.json['product_id']
-            sales_date = request.json['sales_date']
-            total_amount = request.json['total_amount']
-            payment_method = request.json['payment_method']
-            notes = request.json['notes']
+            sales = Sale.query(Sale, Customer, Product).join(
+                Customer, Sale.customer_id == Customer.customer_id).join(
+                Product, Sale.product_id == Product.product_id).all()
+            sales = [{
+                'sale_id': sale[0].sale_id,
+                'customer_id': sale[0].customer_id,
+                'customer_name': sale[1].customer_name,
+                'product_id': sale[0].product_id,
+                'product_name': sale[2].product_name,
+                'quantity': sale[0].quantity,
+                'price': sale[0].price
+            }for sale in sales]
 
-            product = db.session.query(Product).filter_by(
-                product_id=product_id).first()
-
-            sale = Sale(
-                customer_id=customer_id,
-                product_id=product_id,
-                sales_date=sales_date,
-                total_amount=total_amount,
-                payment_method=payment_method,
-                notes=notes
+            product = Product(
+                product_name=request.json['product_name'],
+                price=request.json['price'],
+                stock_quantity=request.json['stock_quantity'],
+                barcode=request.json['barcode'],
+                category=request.json['category']
             )
-
-            db.session.add(sale)
+            db.session.add(product)
             db.session.commit()
-
-            return jsonify({
-                'sale_id': sale.sale_id,
-                'customer_id': sale.customer_id,
-                'product_id': sale.product_id,
-                'sales_date': sale.sales_date,
-                'total_amount': sale.total_amount,
-                'payment_method': sale.payment_method,
-                'notes': sales.notes,
-                'product': {
-                    'product_id': product.product_id,
-                    'product_name': product.product_name,
-                    'price': product.price,
-                    'quantity': product.quantity
-                }
-            })
         except Exception as e:
-            return {'error': str(e)}
-
-    def put(self):
-        try:
-            sale_id = request.json['sale_id']
-            customer_id = request.json['customer_id']
-            product_id = request.json['product_id']
-            sales_date = request.json['sales_date']
-            total_amount = request.json['total_amount']
-            payment_method = request.json['payment_method']
-            notes = request.json['notes']
-
-            sale = db.session.query(Sale).filter_by(
-                sale_id=sale_id).first()
-
-            sale.customer_id = customer_id
-            sale.product_id = product_id
-            sale.sales_date = sales_date
-            sale.total_amount = total_amount
-            sale.payment_method = payment_method
-            sale.notes = notes
-
-            db.session.commit()
-
-            return jsonify({
-                'sale_id': sale.sale_id,
-                'customer_id': sale.customer_id,
-                'product_id': sale.product_id,
-                'sales_date': sale.sales_date,
-                'total_amount': sale.total_amount,
-                'payment_method': sale.payment_method,
-                'notes': sale.notes
-            })
-        except Exception as e:
-            return {'error': str(e)}
+            return str(e)
